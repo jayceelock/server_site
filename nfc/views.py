0d24be7f4288890f135f1ef1ca8161fd5b5d637f
jayceelock <jaycee.lock@gmail.com>
2013-09-05 14:47:59 +0200

    Made changes to nfc app and added security keys

--- nfc/views.py
+++ nfc/views.py
@@ -2,16 +2,19 @@ from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from display_qrcode.models import ProductData
 
+from Crypto.PublicKey import RSA
+import base64
+
 def getData(request):
     
     request.session.set_expiry(1)
     
     username = request.GET['user']
-    encoded_password = request.GET['password']
+    encrypted_password = request.GET['password']
     product = request.GET['product']
     
-    #password = affine_decrypt(encoded_password);
-    password = request.GET['password']
+    password = decrypt(encrypted_password);
+    #password = request.GET['password']
     user = authenticate(username=username, password=password)
     
     if user is not None:
@@ -28,42 +31,7 @@ def getData(request):
     else:
         return HttpResponse("Account name and password are incorrect.")
     
-    #return HttpResponse(request.session['username'] + " " + request.session['password'])
-    
-
-def affine_decrypt(encoded_password):
-    
-    alpha = ('a','b','c','d','e','f','g','h'
-                ,'i','j','k','l','m','n','o','p','q'
-                ,'r','s','t','u','v','w','x','y','z')
-    
-    a_char = encoded_password[0]
-    b_char = encoded_password[1]
-    a_num = alpha.index(a_char)
-    b_num = alpha.index(b_char)
-    
-    encrypted_pass_text = encoded_password[2:]
-    
-    a_num %= 26;
-    
-    for x in range(25):
-        if (((a_num * x) % 26) == 1):
-            a = x
-    
-    y = []
-    cypher_index = []
-    decrypted_text = []
-            
-    for x in range(len(encrypted_pass_text)):
-        y.append(alpha.index(encrypted_pass_text[x]))       
-            
-    for x in range(len(encrypted_pass_text)):
-        cypher_index.append((a * (y[x] - b_num)) % 26)
-    
-    for x in range(len(encrypted_pass_text)):
-        decrypted_text.append(alpha[cypher_index[x]])
-            
-    return ''.join(decrypted_text)           
+    #return HttpResponse(request.session['username'] + " " + request.session['password'])           
     
 def check_balance(request, product):
     
@@ -89,3 +57,17 @@ def check_balance(request, product):
     
     else:
         return 0
+    
+    
+def decrypt(encoded_text):
+    f = open('mykey.pem','r')
+    priv_key = RSA.importKey(f.read())
+    f.close()
+    
+    #pub_key = priv_key.publickey()
+    
+    encrypted_text = base64.b64decode(encoded_text)
+    
+    plain_text = priv_key.decode(encrypted_text)
+    
+    return plain_text
